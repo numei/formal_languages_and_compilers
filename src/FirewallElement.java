ba899ab680e947c65e1c71bff365fd0db16fd60b
numei <2257894902@qq.com>
2026-05-21 17:30:55 +0200

    feat: get mininum set of xsd

--- src/FirewallElement.java
+++ src/FirewallElement.java
@@ -1,8 +1,4 @@
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 @XmlRootElement(name = "elements")
 @XmlAccessorType(XmlAccessType.FIELD)
